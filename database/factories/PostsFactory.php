840c414b8c313f59a6b3d6ae66f565c0d089d01a
tihiro <iuetihiro@gmail.com>
2025-10-07 16:16:14 +0900

    Initial commit

---
+++ database/factories/PostsFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Customer;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\posts>
+ */
+class PostsFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'customer_id' => Customer::factory(),
+            'post_date' => $this->faker->dateTimeThisDecade(),
+            'content' => $this->faker->lines()
+        ];
+    }
+}
