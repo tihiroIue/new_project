30ed910a1273059d8ab16a32ab10febcf86a9aa0
tihiro <iuetihiro@gmail.com>
2025-10-19 05:05:06 +0900

    Dockerfil for the mysql database

---
+++ database/factories/ContentFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Content>
+ */
+class ContentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'customer_id' => null,
+            'post_date' => $this->faker->date(),
+            'content' => $this->faker->text()
+        ];
+    }
+}
---
+++ database/seeders/ContentSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Content;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ContentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Content::factory()
+            ->count(30)
+            ->create();
+    }
+}
